4183f4c9739e9eb1828568eb58099a5b4a7ccc3d
CoraHo <cora9122@gmail.com>
2023-07-17 18:05:23 +0800

    Added check out function along with related redux data flow and thanks page.

--- src/data/RestDataSource.js
+++ src/data/RestDataSource.js
@@ -3,9 +3,15 @@ import Axios from "axios";
 
 export class RestDataSource{
 
+    constructor(err_handler) {
+        this.error_handler = err_handler || (() => {});
+    }
+
     GetData = (dataType, params) => this.SendRequest("get", RestUrls[dataType], params);
+
+    StoreData = (dataType, data) => this.SendRequest("post", RestUrls[dataType], {}, data);
 
     /** axios.request returns a promise to loadData in ActionCreator.js.
      */
-    SendRequest = (method, url, params) => Axios.request({method, url, params});
+    SendRequest = (method, url, params, data) => Axios.request({method, url, params, data});
 }
